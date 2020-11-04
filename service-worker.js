1badac4e1eb2ffa1a9a64aac0475ccf152a3c178
jafar pahrudin <jafar.pahrudin@gmail.com>
2020-11-04 15:12:59 +0700

    #2 add push notif

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "footballinfo-v1.0.24";
+const CACHE_NAME = "footballinfo-v1.0.35";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -13,6 +13,7 @@ const urlsToCache = [
   "/src/pages/standings.html",
   "/src/img/background.jpg",
   "/src/img/icon.png",
+  "/src/img/icon.ico",
   "/src/img/icons.woff2",
   "/src/img/jumbotron-back.jpg",
   "/src/img/side-back.jpg",
@@ -58,5 +59,30 @@ self.addEventListener("activate", function (event) {
         })
       );
     })
+  );
+});
+
+self.addEventListener('push', event => {
+  const title = "Football Info";
+
+  let body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = 'Push message no payload';
+  }
+
+  const options = {
+    body: body,
+    icon: "/src/img/icon.ico",
+    badge: "/src/img/icon.ico",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification(title, options)
   );
 });
